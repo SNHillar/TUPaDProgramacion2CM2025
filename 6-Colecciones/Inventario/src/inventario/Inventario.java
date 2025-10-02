96505b396c9e5e203d580099a5100b0e557d94d1
SNHillar <saulhillar@outlook.es>
2025-10-02 20:59:51 -0300

    changes

--- 6-Colecciones/Inventario/src/inventario/Inventario.java
+++ 6-Colecciones/Inventario/src/inventario/Inventario.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class Inventario {
     
+    // 
     private List<Producto> productos = new ArrayList<>();
     
     public void agregarProducto(Producto p){
@@ -16,6 +17,7 @@ public class Inventario {
     }
     
     public void listarProductos(){
+        // recorremos toda la lista con forEach
         for(Producto p : productos){
             System.out.println(p);
         }
@@ -24,8 +26,11 @@ public class Inventario {
     public Producto buscarProductoPorId(String id){
         Producto productoEncontrado = null;
         int i = 0;
+        // while con length de la lista y entra si sigue siendo null
         while(i < productos.size() && productoEncontrado == null){
+            //obtengo la posicion de la lista
             Producto producto = this.productos.get(i);
+            // si el id es igual al de la posicion, retorno el producto 
             if(producto.getId().equalsIgnoreCase(id)){
                 System.out.println("Producto Encontrado: " + producto);
                 return producto;
@@ -33,23 +38,27 @@ public class Inventario {
             i++;
         }
         System.out.println("No se encontro el producto con id: " + id);
+        // devuelve null.
         return productoEncontrado;
     }
     
     
     public void eliminarProducto(String id){
+        // puedo pasar el id con el metodo buscarProducto
         Producto productoABorrar = buscarProductoPorId(id);
         System.out.println("Producto Eliminado.");
+        //borramos el producto
         this.productos.remove(productoABorrar);
     }
     
     public void actualizarStock(String id, int cantidad){
-        
+        // con el metodo lo buscamos por id
         Producto productoStockActualizar = buscarProductoPorId(id);
         
         if(productoStockActualizar != null){
+            // el nuevo stock va a ser lo que tengo, mas la cantidad a sumar (si es negativo lo resta)
             int nuevoStock = productoStockActualizar.getCantidad() + cantidad;
-        
+            // verificamos que el nuevo stock no sea mayor a lo que ya tengo (es decir que no se pueda descontar lo que no existe)
             if(nuevoStock < 0){
                 System.out.println("No hay suficiente stock para descontar " + (-cantidad) + " unidades.\nEl stock actual es: " + productoStockActualizar.getCantidad());
             }else{
@@ -63,6 +72,7 @@ public class Inventario {
     
     
     public void obtenerTotalStock(){
+        // recorro cada item con forEach y me traigo la cantidad con el getter.
         for(Producto p : productos){
             System.out.println("Producto: " + p.getNombre() + " ------------ Stock: " + p.getCantidad());
         }
@@ -77,6 +87,7 @@ public class Inventario {
     public void filtrarPorCategoria(CategoriaProducto categoria) {
     if (categoria != null) {
         boolean encontrado = false; // solo por si no encuentra nada
+        // recorremos la lista completa porque la necesitamos
         for (Producto p : productos) {
             if (p.getCategoria() == categoria) {
                 System.out.println(p);
